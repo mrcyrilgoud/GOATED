3f4e8aad4f0f8cfcda989ef0a7c73ae001d8d237
mrcyrilgoud <mrcyrilgoud@gmail.com>
2022-04-03 17:37:38 -0700

    The GOATED project

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import '@firebase/auth';
 import '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAOWHBpPhKoNhcGFKHH_Q_0AtL2gV-imgQ',
-  authDomain: 'production-a9404.firebaseapp.com',
-  databaseURL: 'https://production-a9404.firebaseio.com',
-  projectId: 'production-a9404',
-  storageBucket: 'production-a9404.appspot.com',
-  messagingSenderId: '525472070731',
-  appId: '1:525472070731:web:ee873bd62c0deb7eba61ce',
+  apiKey: "AIzaSyCCc_eop-KQmd1QHvBjjnONdjA8LjHxDSs",
+  authDomain: "goated-50aec.firebaseapp.com",
+  projectId: "goated-50aec",
+  storageBucket: "goated-50aec.appspot.com",
+  messagingSenderId: "145592818913",
+  appId: "1:145592818913:web:3a7b915f30b59a528f2ba0",
+  measurementId: "G-45FS9JYCTB"
 };
 
 if (!firebase.apps.length) {
